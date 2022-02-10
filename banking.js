d480165063ef2a81af30ae2ffd7e7df7de56c672
mostofa kamal <mostofakamal1990@gmail.com>
2022-02-10 09:49:09 +0600

    withdraw balance calculation

--- banking.js
+++ banking.js
@@ -20,4 +20,32 @@ document.getElementById('deposit-btn').addEventListener('click',function(e){
 
     depositInput.value = '';
     
+});
+
+
+// handle widthdraw 
+
+document.getElementById('withdraw-btn').addEventListener('click', function(e){
+
+  const widthdrawBalanceText = document.getElementById('widthdrawBalance');
+  const amountWithdrawFieldText = document.getElementById('amont-withdraw');
+  const getBalanceText = document.getElementById('balanceTK');
+  const parseWithdrawBalance = parseFloat(widthdrawBalanceText.innerText);
+  const parseAmountWithdrawField = parseFloat(amountWithdrawFieldText.value);
+  const parseBalance = parseFloat(getBalanceText.innerText);
+   
+
+
+  let totalBalance = parseBalance - parseAmountWithdrawField;
+  if(totalBalance < 0) return ;
+
+  getBalanceText.innerText = totalBalance;
+  widthdrawBalanceText.innerText = parseWithdrawBalance +  parseAmountWithdrawField;
+  // clear filed
+
+  amountWithdrawFieldText.value = '';
+
+
+
+
 })
