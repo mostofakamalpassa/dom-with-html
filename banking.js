2ea690dfccb55542e31b98b2c7bbbe49b311a253
mostofa kamal <mostofakamal1990@gmail.com>
2022-02-10 09:18:01 +0600

    calculate diposite

--- banking.js
+++ banking.js
@@ -1,6 +1,7 @@
 //deposite event handalers
 
 document.getElementById('deposit-btn').addEventListener('click',function(e){
+   
     const depositInput = document.getElementById('amount-deposit');
     const depost = document.getElementById('deposit');
   
@@ -8,6 +9,15 @@ document.getElementById('deposit-btn').addEventListener('click',function(e){
     total = parseInt(depositInput.value) + parseInt(depost.innerText);
     depost.innerText = total;
    // return total;
-    
+    // get balance 
+
+    const balnace = document.getElementById('balanceTK');
+    const getbalance = parseFloat(balnace.innerText);
+
+    balnace.innerText = parseInt(depositInput.value) + getbalance;
+
+    // clear input 
+
+    depositInput.value = '';
     
 })
